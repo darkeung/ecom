394e5d163fa407ae209d76d1bf524f4fd6807345
DARKEUNG <hawygfau@gmail.com>
2018-12-18 11:22:39 +0700

    pmy

--- laravel-ecommerce-example/resources/views/about.blade.php
+++ laravel-ecommerce-example/resources/views/about.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 
     @component('components.breadcrumbs')
-       <div>A shopping website create by Woody NG, template by drehimself, create tool by laravel.
+       <div>A shopping cart website create by Woody NG, template by drehimself, create tool by laravel.
         <br>2018 IT Internship in Vietnam </div>
     @endcomponent
 
--- laravel-ecommerce-example/resources/views/blog.blade.php
+++ laravel-ecommerce-example/resources/views/blog.blade.php
@@ -1,100 +1,56 @@
 <!DOCTYPE html>
 <html>
-<head>
+<title>Blog</title>
+<meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <style>
-* {
-  box-sizing: border-box;
-}
-
-/* Add a gray background color with some padding */
-body {
-  font-family: Arial;
-  padding: 20px;
-  background: #f1f1f1;
-}
-
-/* Header/Blog Title */
-.header {
-  padding: 30px;
-  font-size: 40px;
-  text-align: center;
-  background: white;
-}
-
-/* Create two unequal columns that floats next to each other */
-/* Left column */
-.leftcolumn {   
-  float: left;
-  width: 75%;
-}
-
-/* Right column */
-.rightcolumn {
-  float: left;
-  width: 25%;
-  padding-left: 20px;
-}
-
-/* Fake image */
-.fakeimg {
-  background-color: #aaa;
-  width: 100%;
-  padding: 20px;
-}
-
-/* Add a card effect for articles */
-.card {
-   background-color: white;
-   padding: 20px;
-   margin-top: 20px;
-}
-
-/* Clear floats after the columns */
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-/* Footer */
-.footer {
-  padding: 20px;
-  text-align: center;
-  background: #ddd;
-  margin-top: 20px;
-}
-
-/* Responsive layout - when the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other */
-@media screen and (max-width: 800px) {
-  .leftcolumn, .rightcolumn {   
-    width: 100%;
-    padding: 0;
-  }
-}
+body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
 </style>
-</head>
-<body>
+<body class="w3-light-grey">
 
-<div class="header"><div class="fakeimg">
-  <h2><a href="/">Ecommerce</a></h2>
-</div>
-</div>
-<div class="row">
-  <div class="leftcolumn">
-     <div class="card">
-      <h2>BLACK FRIDAY!</h2>
-      <h5>Only 1 Day of BLACK FRIDAY Sale!, Dec 21, 2018</h5>
-      <img src="img/6p2p0003sr6786224o18.jpg" alt="black friday image">
-      <p>All of things 50% off!!!</p>
-      <p>The BLACK FRIDAY specials are finally here.</p>
+<!-- w3-content defines a container for fixed size centered content, 
+and is wrapped around the whole page content, except for the footer in this example -->
+<div class="w3-content" style="max-width:1400px">
+
+<!-- Header -->
+<header class="w3-container w3-center w3-padding-32"> 
+  <h1><b><a href="/">Ecommerce</a></b></h1>
+  <p>Welcome to the blog of <span class="w3-tag">Ecommerce</span></p>
+</header>
+
+<!-- Grid -->
+<div class="w3-row">
+
+<!-- Blog entries -->
+<div class="w3-col l8 s12">
+  <!-- Blog entry -->
+  <div class="w3-card-4 w3-margin w3-white">
+    <img src="img/6p2p0003sr6786224o18.jpg" alt="Nature" style="width:100%">
+    <div class="w3-container">
+      <h3><b>BLACK FRIDAY!</b></h3>
+      <h5>Only 1 Day of BLACK FRIDAY Sale!, <span class="w3-opacity">December 21, 2018</span></h5>
     </div>
 
-    <div class="card">
-      <h2>Boxing Day!</h2>
-      <h5>Only This Day, Dec 19, 2018</h5>
-       <img src="img/eb-games-boxing-week-2014.png" alt="boxing image">
-      <p>Boxing Day-Trade 5 game gets $100 to buy a new PS4 console.</p>
+    <div class="w3-container">
+      <p>All of things 50% off!!!<br>
+      The BLACK FRIDAY specials are finally here.</p>
+      
+    </div>
+  </div>
+  <hr>
+
+  <!-- Blog entry -->
+  <div class="w3-card-4 w3-margin w3-white">
+  <img src="img/eb-games-boxing-week-2014.png" alt="Norway" style="width:100%">
+    <div class="w3-container">
+      <h3><b>Boxing Day!</b></h3>
+      <h5>Boxing Day-Trade 5 game gets $100 to buy a new PS4 console, <span class="w3-opacity">December 19, 2018</span></h5>
+    </div>
+
+    <div class="w3-container">
+   
       <p>Boxing Day offers that game fans are looking forward to have been released! All goods can be purchased through the official storefront.<br>
 
 It can be purchased online through the official website or through the official storefront, but the time of the two purchases varies.<br>
@@ -105,37 +61,81 @@ To purchase through the official website, you can start buying immediately on th
 Offer cannot be mixed with other offers.<br>
 
 TRADE IN 5 Games can get $100 and is used to purchase the PS4.</p>
-    </div>
-    <div class="card">
-      <h2>Xmas Best Deals!</h2>
-      <h5>FINAL 1 WEEK of Xmas On All Our Best Deals!, Dec 17, 2018</h5>
-      <img src="img/5c0df54be763a752705727.jpg" alt="xmas image">
-      <p>Last Chance! Ultimate Christmas Gift Guide.</p>
-      <p>The long-awaited Christmas specials are finally here. This wave of killing did not disappoint everyone!</p>
+     
     </div>
   </div>
-  <div class="rightcolumn">
-    <div class="card">
-      <h2>About Me</h2>
-      <div class="fakeimg" style="height:100px;">Ecommerce</div>
-      <p>A Shopping website.</p>
+<hr>
+  <!-- Blog entry -->
+  <div class="w3-card-4 w3-margin w3-white">
+  <img src="img/5c0df54be763a752705727.jpg" alt="Norway" style="width:100%">
+    <div class="w3-container">
+      <h3><b>Xmas Best Deals!</b></h3>
+      <h5>FINAL 1 WEEK of Xmas On All Our Best Deals!, <span class="w3-opacity">December 17, 2018</span></h5>
     </div>
-    <div class="card">
-      <h3>Popular Post</h3>
-      <div class="fakeimg">BLACK FRIDAY!</div><br>
-      <div class="fakeimg">Boxing Day!</div><br>
-      <div class="fakeimg">Xmas Best Deals!</div>
-    </div>
-    <div class="card">
-      <h3>Follow Me</h3>
-      <p>Some text..</p>
+
+    <div class="w3-container">
+   
+     <p>Last Chance! Ultimate Christmas Gift Guide.<br>
+      The long-awaited Christmas specials are finally here. This wave of killing did not disappoint everyone!</p>
+     
     </div>
   </div>
+<!-- END BLOG ENTRIES -->
 </div>
 
-<div class="footer">
-  <h2>Footer</h2>
+<!-- Introduction menu -->
+<div class="w3-col l4">
+  <!-- About Card -->
+  <div class="w3-card w3-margin w3-margin-top">
+  <img src="img/71Kchra215L._AC_.jpg" style="width:100%">
+    <div class="w3-container w3-white">
+      <h4><b>About Us</b></h4>
+      <p>Ecommerce</p>
+      <p>A Shopping website.</p>
+    </div>
+  </div><hr>
+  
+  <!-- Posts -->
+  <div class="w3-card w3-margin">
+    <div class="w3-container w3-padding">
+      <h4>Popular Posts</h4>
+    </div>
+    <ul class="w3-ul w3-hoverable w3-white">
+      <li class="w3-padding-16">
+        <img src="img/6p2p0003sr6786224o18.jpg" alt="Image" class="w3-left w3-margin-right" style="width:50px">
+        <span class="w3-large">BLACK FRIDAY!</span><br>
+        <span>Left side...</span>
+      </li>
+      <li class="w3-padding-16">
+        <img src="img/eb-games-boxing-week-2014.png" alt="Image" class="w3-left w3-margin-right" style="width:50px">
+        <span class="w3-large">Boxing Day!</span><br>
+        <span>Left side...</span>
+      </li> 
+      <li class="w3-padding-16">
+        <img src="img/5c0df54be763a752705727.jpg" alt="Image" class="w3-left w3-margin-right" style="width:50px">
+        <span class="w3-large">Xmas Best Deals!</span><br>
+        <span>Left side...</span>
+      </li>   
+    </ul>
+  </div>
+  <hr> 
+ 
+  
+  
+<!-- END Introduction Menu -->
 </div>
+
+<!-- END GRID -->
+</div><br>
+
+<!-- END w3-content -->
+</div>
+
+<!-- Footer -->
+<footer class="w3-container w3-dark-grey w3-padding-32 w3-margin-top">
+ 
+  <p>Powered by <a href="#" target="_blank">w3.css</a></p>
+</footer>
 
 </body>
 </html>
